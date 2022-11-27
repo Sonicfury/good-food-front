deb9d1f6293545d6979a66cdbf7b59836671aa71
Sonicfury <theo.lambrt@gmail.com>
2022-11-27 22:36:09 +0100

    chore: lint with prettier

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -1,8 +1,8 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
- 
-export const POST:RequestHandler = async ({ cookies, request,  }) => {
-  const data = await request.json();
+
+export const POST: RequestHandler = async ({ cookies, request }) => {
+    const data = await request.json();
     const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
         method: 'POST',
         body: JSON.stringify(data),
@@ -10,12 +10,11 @@ export const POST:RequestHandler = async ({ cookies, request,  }) => {
             'content-type': 'application/json',
         },
     });
-    const requestBody = await  response.json()
-    if(requestBody.success){
-      cookies.set('session', requestBody.data.token)
-      return  new Response(JSON.stringify({data: requestBody.data}), { status: 200 })
-    }else{
-      return new Response(JSON.stringify({error: requestBody.data.error}), { status: 500 });
+    const requestBody = await response.json();
+    if (requestBody.success) {
+        cookies.set('session', requestBody.data.token);
+        return new Response(JSON.stringify({ data: requestBody.data }), { status: 200 });
+    } else {
+        return new Response(JSON.stringify({ error: requestBody.data.error }), { status: 500 });
     }
-
-}
+};
