ec71eec4bb05221ee3c8bbaa5929f2111aa6de5e
Sonicfury <theo.lambrt@gmail.com>
2022-11-27 22:10:08 +0100

    chore: code format with prettier

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,8 +1,8 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
- 
+
 export const POST: RequestHandler = async ({ cookies, request }) => {
-  const data = await request.json();
+    const data = await request.json();
     const response = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
         method: 'POST',
         body: JSON.stringify(data),
@@ -10,13 +10,15 @@ export const POST: RequestHandler = async ({ cookies, request }) => {
             'content-type': 'application/json',
         },
     });
-    const requestBody = await  response.json()
-    if(requestBody.success){
-      cookies.set('session', requestBody.data.token)
-      return  new Response(JSON.stringify({data: requestBody.data}), { status: 200 })
-    }else if(requestBody.message === "Validation Error."){
-      return new Response(JSON.stringify({message: "Un compte avec cette email est déjà enregistré"}), { status: 500 });
-    }else{
-      return new Response(JSON.stringify({message: "Une erreur est survenue"}), { status: 500 });
+    const requestBody = await response.json();
+    if (requestBody.success) {
+        cookies.set('session', requestBody.data.token);
+        return new Response(JSON.stringify({ data: requestBody.data }), { status: 200 });
+    } else if (requestBody.message === 'Validation Error.') {
+        return new Response(JSON.stringify({ message: 'Un compte avec cette email est déjà enregistré' }), {
+            status: 500,
+        });
+    } else {
+        return new Response(JSON.stringify({ message: 'Une erreur est survenue' }), { status: 500 });
     }
-}
+};
