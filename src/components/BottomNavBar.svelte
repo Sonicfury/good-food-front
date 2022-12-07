28519a1d84e1235ed7be29d495ff81f62a2c816a
Sonicfury <theo.lambrt@gmail.com>
2022-12-07 18:16:07 +0100

    feat: replaced mdi-svelte by custom component
    implemented a custom component handling mdi icons and replaced usage in BottomNavBar.svelte

---
+++ src/components/BottomNavBar.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import { mdiMapMarkerOutline } from '@mdi/js';
+    import { mdiHomeOutline } from '@mdi/js';
+    import { mdiAccountOutline } from '@mdi/js';
+    import Icon from './Icon.svelte';
+    import { page } from '$app/stores';
+
+    const isActive = (path: string) => $page.route.id === path;
+</script>
+
+<div class="fixed bottom-4 transform inline-flex  mx-auto justify-between bg-white w-full bottom-0">
+    <a
+        aria-current="page"
+        class="inline-flex flex-col items-center text-primary text-xs font-medium py-3 px-4 text-white flex-grow"
+        href="/app/dashboard"
+    >
+        <Icon path="{mdiHomeOutline}" clazz="{isActive('/app/dashboard') ? 'fill-primary' : 'fill-secondary'}" />
+        <span class="sr-only">Home</span>
+    </a>
+
+    <a class="relative inline-flex flex-col items-center text-xs font-medium text-white py-3 px-6 flex-grow">
+        <div class="absolute bottom-5 p-3 rounded-full border-4 border-base-100 bg-accent">
+            <Icon path="{mdiMapMarkerOutline}" clazz="fill-white w-10 h-10" />
+        </div>
+        <span class="sr-only">Location</span>
+    </a>
+    <a
+        class="inline-flex flex-col items-center text-secondary text-xs font-medium text-blue-400 py-3 px-4 flex-grow"
+        href="/app/profile"
+    >
+        <Icon path="{mdiAccountOutline}" clazz="{isActive('/app/profile') ? 'fill-primary' : 'fill-secondary'}" />
+        <span class="sr-only">Profile</span>
+    </a>
+</div>
