8a2fd401ebbf079bec5e22ef12be82d99c97c5ce
Sonicfury <theo.lambrt@gmail.com>
2022-12-08 20:34:17 +0100

    test: added dashboard and login tests
    also added test-user fixture

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -1,21 +1,18 @@
-import * as path from 'path';
+import * as path from 'path'
 
-const url = 'http://localhost:5173';
+const url = 'http://localhost:5173'
 
 describe('Dashboard', () => {
-    it('should display a navbar with 3 elements', () => {
-        cy.visit(new URL(path.join('app', 'dashboard'), url).href);
+  it('should display a navbar with 3 elements', () => {
+    cy.visit(new URL(path.join('app', 'dashboard'), url).href)
 
-        cy.get('nav').should('be.visible').find('a').should('have.length', 3);
-    });
+    cy.get('nav').should('be.visible').find('a').should('have.length', 3)
+  })
 
-    it('should have the home button active', () => {
-        cy.visit(new URL(path.join('app', 'dashboard'), url).href);
-        cy.get('a[aria-current="page"]')
-            .should('be.visible')
-            .should('have.attr', 'href')
-            .and('include', '/app/dashboard');
-    });
-});
+  it('should have the home button active', () => {
+    cy.visit(new URL(path.join('app', 'dashboard'), url).href)
+    cy.get('a[aria-current="page"]').should('be.visible').should('have.attr', 'href').and('include', '/app/dashboard')
+  })
+})
 
-export {};
+export {}
