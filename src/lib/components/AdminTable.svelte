758c4ecefcbb1b2326a0117e3d21f28a024ed8e5
justine-dequidt-gyb <justine.dequidt@viacesi.fr>
2023-01-30 16:45:25 +0100

    feat(48): add edit and delete category

--- src/lib/components/AdminTable.svelte
+++ src/lib/components/AdminTable.svelte
@@ -7,6 +7,9 @@
   import type { Category } from '$lib/models/category'
   import { paginate, LightPaginationNav } from 'svelte-paginate'
   import Search from './Search.svelte'
+  import { createEventDispatcher } from 'svelte'
+  import Modal from './Modal.svelte'
+  const dispatch = createEventDispatcher()
 
   export let carteItemName: string
   export let cartefetchItem: Array<Category>
@@ -16,7 +19,10 @@
   let currentPage: number = 1
   let pageSize: number = 5
   let paginatedItems: Array<Category> = []
-  let searchTerm = ''
+  let searchTerm: string = ''
+  let itemToPut = null
+  let showModal: boolean = false
+  let editItem = {}
 
   $: if (cartefetchItem.length > 0) {
     items = cartefetchItem
@@ -30,13 +36,37 @@
     })
     paginatedItems = paginate({ items, pageSize, currentPage })
   }
+
+  const deleteItem = async (id: Number) => {
+    dispatch('deleteItem', {
+      id: id,
+    })
+  }
+
+  function putItem(carteItem: Category) {
+    if (carteItem.id) {
+      dispatch('editItem', {
+        item: carteItem,
+      })
+    } else {
+      dispatch('postItem', {
+        item: carteItem,
+      })
+    }
+    showModal = false
+  }
+
+  function openModalWithItem(item: Category) {
+    editItem = item
+    showModal = true
+  }
 </script>
 
 <div class=" flex justify-around mt-24 mb-4">
-  <label for="my-modal-5" class="btn btn-accent text-white"> + Ajouter un/une {carteItemName} </label>
+  <button on:click="{() => (showModal = true)}" class="btn btn-accent text-white">
+    + Ajouter un/une {carteItemName}
+  </button>
   <Search bind:searchTerm="{searchTerm}" on:input="{search}" placeholder="Recherche ..." />
-
-  <CarteForm carteItemName="{carteItemName}" />
 </div>
 <div class="overflow-x-auto flex justify-center">
   <table class="table w-3/4">
@@ -53,23 +83,31 @@
           <th>{carteItem.id}</th>
           <td>{carteItem.name}</td>
           <td class="flex content-row">
-            <label class="mr-8" for="my-modal-5"
-              ><Icon
+            <button on:click="{() => openModalWithItem(carteItem)}">
+              <Icon
                 path="{mdiCircleEditOutline}"
                 clazz="{isActive('/admin/carte') ? 'fill-success' : 'fill-secondary'}"
-              /></label
-            >
-            <button>
+              />
+            </button>
+            <button on:click="{() => deleteItem(carteItem.id)}">
               <Icon path="{mdiDeleteOutline}" clazz="{isActive('/admin/carte') ? 'fill-error' : 'fill-secondary'}" />
             </button>
           </td>
         </tr>
+        {#if showModal}
+          <Modal>
+            <CarteForm carteItemName="{carteItemName}" item="{editItem}" />
+            <div class=" flex content-row justify-center m-6">
+              <button on:click="{() => (showModal = false)}" class="btn btn-error text-white m-2">Annuler</button>
+              <button on:click="{() => putItem(editItem)}" class="btn btn-success text-white m-2"> Valider</button>
+            </div>
+          </Modal>
+        {/if}
       {/each}
     </tbody>
   </table>
-
-  <CarteForm carteItemName="{carteItemName}" />
 </div>
+
 <div class="pagination-nav">
   <LightPaginationNav
     totalItems="{items.length}"
@@ -85,5 +123,8 @@
   .pagination-nav :global(.pagination-nav) {
     background: none !important;
     box-shadow: none !important;
+  }
+  .table th:first-child {
+    position: static;
   }
 </style>
--- src/routes/admin/carte/+page.svelte
+++ src/routes/admin/carte/+page.svelte
@@ -4,6 +4,10 @@
   import { onMount } from 'svelte'
   let carteItemName: string = 'categories'
   $: cartefetchItem = [] as Array<Category>
+  import Alert from '$lib/components/Alert.svelte'
+
+  let messageError: string = ''
+  let isMessageError: boolean = false
 
   onMount(async () => {
     getCarteItem(carteItemName)
@@ -15,7 +19,55 @@
     let response = await res.json()
     if (response.data) {
       cartefetchItem = response.data
+      cartefetchItem.sort((a, b) => b.id - a.id)
     }
+  }
+  const deleteItem = async (event) => {
+    const res = await fetch(`/api/${carteItemName}`, {
+      method: 'DELETE',
+      body: JSON.stringify(event.detail.id),
+    })
+    let response = await res.json()
+    if (response.data) {
+      isMessageError = true
+      messageError = 'Supprimé avec suuccès'
+    } else {
+      isMessageError = true
+      messageError = 'Une erreur est survenue'
+    }
+    getCarteItem(carteItemName)
+  }
+
+  const editItem = async (event) => {
+    const res = await fetch(`/api/${carteItemName}`, {
+      method: 'PUT',
+      body: JSON.stringify(event.detail.item),
+    })
+    let response = await res.json()
+    if (response.data) {
+      isMessageError = true
+      messageError = 'Modifié avec suuccès'
+    } else {
+      isMessageError = true
+      messageError = 'Une erreur est survenue'
+    }
+    getCarteItem(carteItemName)
+  }
+
+  const postItem = async (event) => {
+    const res = await fetch(`/api/${carteItemName}`, {
+      method: 'POST',
+      body: JSON.stringify(event.detail.item),
+    })
+    let response = await res.json()
+    if (response.data) {
+      isMessageError = true
+      messageError = 'Ajouté avec suuccès'
+    } else {
+      isMessageError = true
+      messageError = 'Une erreur est survenue'
+    }
+    getCarteItem(carteItemName)
   }
 </script>
 
@@ -47,7 +99,14 @@
     >
   </div>
 </div>
-<AdminTable carteItemName="{carteItemName}" cartefetchItem="{cartefetchItem}" />
+<Alert isError="{isMessageError}" message="{messageError}" />
+<AdminTable
+  on:deleteItem="{deleteItem}"
+  on:editItem="{editItem}"
+  on:postItem="{postItem}"
+  carteItemName="{carteItemName}"
+  cartefetchItem="{cartefetchItem}"
+/>
 
 <style>
   .tab.tab-active {
