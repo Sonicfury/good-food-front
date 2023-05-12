63b72dbf603fef887862554a3fb352680b226fbc
Sonicfury <theo.lambrt@gmail.com>
2023-05-12 22:05:14 +0200

    feat: added blank service worker

---
+++ src/service-worker.ts
@@ -0,0 +1,8 @@
+/// <reference types="@sveltejs/kit" />
+self.addEventListener('install', (event) => {
+    event.waitUntil(console.log('installed'));
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(console.log('activated'));
+});
