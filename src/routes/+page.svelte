876e40769be2523c4be0f51011716445b309514f
Sonicfury <theo.lambrt@gmail.com>
2023-05-22 11:54:06 +0200

    fix: added root component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,20 @@
   import { isAuthenticated } from '$lib/helpers/guard'
   import { goto } from '$app/navigation'
   import { onMount } from 'svelte'
+  import {fade} from 'svelte/transition';
 
-  onMount(() => {
+  const timeout = (ms: string) => new Promise(resolve => setTimeout(resolve, ms));
+
+  onMount(async () => {
+    await timeout(1700);
     if (isAuthenticated()) {
-      goto('/app/dashboard')
+      await goto('/app/dashboard')
     }
-    goto('/login')
+    await goto('/login')
     return
   })
 </script>
+
+<div in:fade={{ duration: 1500 }} class="w-full h-screen flex justify-center items-center">
+    <h1 class="text-2xl text-primary">Good food</h1>
+</div>
---
+++ src/routes/pwa/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import {onMount} from 'svelte';
+    import {goto} from '$app/navigation';
+    import {fade} from 'svelte/transition';
+ 
+    const timeout = (ms: string) => new Promise(resolve => setTimeout(resolve, ms));
+    onMount(async () => 
+        {
+            await timeout(1700);
+            await goto('/login');
+        }
+    )
+</script>
+<div in:fade={{ duration: 1500 }} class="w-full h-screen flex justify-center items-center">
+    <h1 class="text-2xl text-primary">Good food</h1>
+</div>
+
+
